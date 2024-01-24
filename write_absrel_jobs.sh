0bc21c403d55c8bf94b98cad6b6fc266338a3dc8
Ekaterina Osipova <ekaterina.osipova@senckenberg.de>
2024-01-24 22:29:25 +0100

    updated command to run absrel

--- write_absrel_jobs.sh
+++ write_absrel_jobs.sh
@@ -18,7 +18,7 @@ do
 			if  grep -q "${t%$suff}" $qual_trans ; then
 				ans_tree=${t%$suff}.ans.tree
 				out_file="out.${t%$suff}.json"
-				echo "hyphy absrel -a $msa_final_dir/$gene/$t -t $absrel_dir/$gene/${t%$suff}/${t%$suff}.ans.tree -o $absrel_dir/$gene/${t%$suff}/$out_file"
+				echo "hyphy absrel --alignment $msa_final_dir/$gene/$t --tree $absrel_dir/$gene/${t%$suff}/${t%$suff}.ans.tree --output $absrel_dir/$gene/${t%$suff}/$out_file"
 			fi
 		done
 	fi
